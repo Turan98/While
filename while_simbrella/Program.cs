b513405fd414fba30ebd7dfeb0e3acd4ac8b31a4
Turan98 <48137040+Turan98@users.noreply.github.com>
2023-02-13 11:43:40 +0400

    while_test_program

---
+++ while_simbrella/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+int odd_num = 0;
+int even_num = 0;
+int sum_odd_num = 0;
+int sum_even_num = 0;
+
+Console.WriteLine("Enter start num: ");
+int start_num = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter last num: ");
+int limit_num = int.Parse(Console.ReadLine());
+
+while (start_num < limit_num)
+{
+    if (start_num % 2 == 0)
+    {
+        sum_even_num += start_num;
+        even_num ++;
+    }
+    else
+    {
+        sum_odd_num += start_num;
+        odd_num ++;
+    }
+   start_num ++; 
+}
+
+Console.WriteLine("Odd num: " + odd_num);
+Console.WriteLine("Even num: " + even_num);
+Console.WriteLine("Sum Odd num: " + sum_odd_num);
+Console.WriteLine("Sum even num: " + sum_even_num);
